f871754a195fb6e30f5e0f6c5316081cd94151cb
ricky0906 <rupinia.ricky@gmail.com>
2021-01-13 18:31:07 +0900

    Revert "usersテーブルの作成"
    This reverts commit 0423c944bfb28f5a86d496b1dce98946da4252fa.

--- db/migrate/20210113085934_devise_create_users.rb
+++ db/migrate/20210113085934_devise_create_users.rb
@@ -4,14 +4,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: "", unique: true
+      t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :nickname,           null: false, default: ""
-      t.string :first_name,         null: false
-      t.string :last_name,          null: false
-      t.string :first_name_kana,    null: false
-      t.string :last_name_kana,     null: false
-      t.date :birthday,             null: false
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
