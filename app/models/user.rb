859651e2960eb38160a9b53d4e80ea831518372c
ricky0906 <rupinia.ricky@gmail.com>
2021-01-18 18:30:30 +0900

    新規登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :password, format: {with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/} # 半角英数字混合での入力必須
   validates :email, uniqueness: true
-  validates :encrypted_password, format: {with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/} # 半角英数字混合での入力必須
-  validates :nickname, presence: true
+  validates :nickname, presence: true, uniqueness: true
   validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/} # 全角での入力必須
   validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/} # 全角での入力必須
   validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/} # 全角カタカナでの入力必須
