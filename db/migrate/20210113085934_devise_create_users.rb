8baad34bf33fc4ecc9b12897f2fb1d52b4696045
ricky0906 <rupinia.ricky@gmail.com>
2021-01-20 10:00:17 +0900

    nicknameカラムを一意性にするため、マイグレーションファイルを修正

--- db/migrate/20210113085934_devise_create_users.rb
+++ db/migrate/20210113085934_devise_create_users.rb
@@ -6,7 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: "", unique: true
       t.string :encrypted_password, null: false, default: ""
-      t.string :nickname,           null: false
+      t.string :nickname,           null: false, default: "", unique: true
       t.string :first_name,         null: false
       t.string :last_name,          null: false
       t.string :first_name_kana,    null: false
