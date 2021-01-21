197a26e89ec26df87221a38fbe8a794f3dcc5de8
ricky0906 <rupinia.ricky@gmail.com>
2021-01-21 21:14:58 +0900

    バリデーションの記述の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :password, length: {minimum: 6}, format: {with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/} # 半角英数字混合での入力必須
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+  validates :password, length: {minimum: 6}
   validates :email, uniqueness: true
   validates :nickname, presence: true, uniqueness: true
-  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/} # 全角での入力必須
-  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/} # 全角での入力必須
-  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/} # 全角カタカナでの入力必須
-  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/} # 全角カタカナでの入力必須
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: {with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください'} do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
   validates :birthday, presence: true 
 end
