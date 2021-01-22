382b4095d7e887ebcdeb95ff9125f261538b641e
ricky0906 <rupinia.ricky@gmail.com>
2021-01-22 11:38:55 +0900

    バリデーションの記述を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,6 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
   validates :password, length: { minimum: 6 }
-  validates :email, uniqueness: true
-  validates :nickname, presence: true, uniqueness: true
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
     validates :first_name
     validates :last_name
@@ -17,5 +15,8 @@ class User < ApplicationRecord
     validates :first_name_kana
     validates :last_name_kana
   end
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :birthday
+    validates :nickname, uniqueness: true
+  end
 end
