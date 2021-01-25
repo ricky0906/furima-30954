5ca1aa8b45f42ad7bb6ae7494b4d689f58788573
ricky0906 <rupinia.ricky@gmail.com>
2021-01-25 10:30:49 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :explanation
+    validates :category_id
+    validates :condition_id
+    validates :shipping_charge_id
+    validates :prefecture_id
+    validates :shipping_days_id
+    validates :price
+    validates :user
+  end
 end
