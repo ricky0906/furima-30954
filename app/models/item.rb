9a4878fc1c9ee1f695ffb4f91c259313370cf281
ricky0906 <rupinia.ricky@gmail.com>
2021-01-28 18:46:00 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,25 @@
 class Item < ApplicationRecord
-  with_options presence: true do
-    validates :name
-    validates :explanation
-    validates :category_id
-    validates :condition_id
-    validates :shipping_charge_id
-    validates :prefecture_id
-    validates :shipping_days_id
-    validates :price
-    validates :user
-  end
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :shipping_day
 
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :name, length: {maximum: 40}
+    validates :explanation, length: {maximum: 1000}
+    validates :price
+    validates :user
+    with_options numericality: {other_than: 1} do
+      validates :category_id
+      validates :condition_id
+      validates :shipping_charge_id
+      validates :prefecture_id
+      validates :shipping_days_id
+    end
+  end
 end
