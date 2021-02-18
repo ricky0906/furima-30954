f0dde17f60710bfe27ee1a69e1823a9c920cc61b
ricky0906 <rupinia.ricky@gmail.com>
2021-02-18 20:41:31 +0900

    クレカ機能の実装

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
+  attr_accessor :token
   has_one :item
   has_one :address
   belongs_to :user
-  belongs_to :prefecture
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building_name, :phone, :user_id, :item_id, :order_id
+  attr_accessor :token, :postal_code, :prefecture_id, :city, :address, :building_name, :phone, :price, :user_id, :item_id, :order_id
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
@@ -8,9 +8,11 @@ class OrderAddress
     validates :city
     validates :address
     validates :phone, format: { with: /\A[0-9]+\z/ }
+    validates :token
   end
 
   def save
+    binding.pry
     order = Order.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, 
                    building_name: building_name, phone: phone, order_id: order.id)
